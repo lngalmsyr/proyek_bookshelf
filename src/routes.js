617ebf7caa12845ff6c52af74a43d4db39d3506b
lngalmsyr <lanang.al2610@gmail.com>
2023-03-25 16:23:14 +0700

    update salah 3

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,35 @@
-const { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler } = require('./handler');
-
-const routes = [
+const { addBookHandler, 
+    getAllBooksHandler,
+    getBookByIdHandler,
+    editBookByIdHandler,
+    deleteBookByIdHandler } = require('./handler');
+  
+  const routes = [
     {
-        method: 'POST',
-        path: '/books',
-        handler: (addBookHandler),
+      method: 'POST',
+      path: '/books',
+      handler: addBookHandler,
     },
     {
-        method: 'GET',
-        path: '/books',
-        handler: (getAllBooksHandler),
+      method: 'GET',
+      path: '/books',
+      handler: getAllBooksHandler,
     },
     {
-        method: 'GET',
-        path: '/books/{bookId}',
-        handler: (getBookByIdHandler),
+      method: 'GET',
+      path: '/books/{id}',
+      handler: getBookByIdHandler,
     },
     {
-        method: 'PUT',
-        path: '/books/{bookId}',
-        handler: (editBookByIdHandler),
+      method: 'PUT',
+      path: '/books/{id}',
+      handler: editBookByIdHandler,
     },
     {
-        method: 'DELETE',
-        path: '/books/{bookId}',
-        handler: (deleteBookByIdHandler),
-
+      method: 'DELETE',
+      path: '/books/{id}',
+      handler: deleteBookByIdHandler,
     },
-
-];
-
-module.exports = routes;
+  ];
+  
+  module.exports = routes;
